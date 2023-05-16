0c80e9f41306b80301f701df6e77689d7f6924b7
Martin Vysny <mavi@vaadin.com>
2023-05-16 13:02:34 +0300

    docs

--- src/main/java/com/vaadin/starter/skeleton/MainView.java
+++ src/main/java/com/vaadin/starter/skeleton/MainView.java
@@ -58,11 +58,17 @@ public class MainView extends VerticalLayout {
         dialog.addCancelListener(e -> responseQueue.add(false));
         dialog.open();
         try {
-            // await until the user clicks a button, which adds a value to the responseQueue.
-            // This is virtual thread and take() unmounts. (see https://blogs.oracle.com/javamagazine/post/java-loom-virtual-threads-platform-threads )
+            // Await until the user clicks a button, which adds a value to the responseQueue.
+            // This only works with virtual threads.
+            //
+            // Since this is virtual thread, take() unmounts instead of blocking the current thread.
+            // See https://blogs.oracle.com/javamagazine/post/java-loom-virtual-threads-platform-threads for more details on the virtual thread basics.
             // That means that while we're 'blocked' in take(), Vaadin UI thread is allowed to finish and render the dialog.
             // On button click, value is added to the responseQueue which wakes up this virtual thread.
             // This virtual thread mounts to a Vaadin UI thread and continues execution.
+            //
+            // If this was not a virtual thread, this would obviously block endlessly and would not draw any dialog.
+            // See https://mvysny.github.io/vaadin-blocking-dialogs/ for more details.
             final Boolean response = responseQueue.take();
 
             // Now we're mounted to a Vaadin UI thread.
