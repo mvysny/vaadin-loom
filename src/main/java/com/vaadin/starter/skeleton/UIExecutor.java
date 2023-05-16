73b02752c8e2f9c875d3166f5d36bf44e8be577d
Martin Vysny <mavi@vaadin.com>
2023-05-16 13:12:52 +0300

    javadoc

--- src/main/java/com/vaadin/starter/skeleton/UIExecutor.java
+++ src/main/java/com/vaadin/starter/skeleton/UIExecutor.java
@@ -68,29 +68,36 @@ public final class UIExecutor implements AutoCloseable {
     private static Thread.Builder.OfVirtual newVirtualBuilder(@NotNull UI ui) {
         Objects.requireNonNull(ui);
         // we'll create a virtual thread builder which runs continuations on the UI thread, via ui.access().
+
+        // first, we'll construct an executor which runs submitted Runnables in the UI thread, via ui.access()
+        final Executor uiExecutor = command -> {
+            // "command" is a continuation which runs another piece of the virtual thread.
+            ui.access((Command) () -> {
+                // current thread will become a carrier thread once command.run() is run;
+                // the command.run() itself will run in a virtual thread.
+                final Thread carrierThread = Thread.currentThread();
+                CURRENT_UI.put(carrierThread, UI.getCurrent());
+                try {
+                    // This calls a JVM Continuation. Upon mounting a continuation,
+                    // the current thread is switched to a virtual thread (!!). The current (carrier) thread is blocked while executing virtual thread,
+                    // which means that the virtual thread also runs in the UI thread and is safe to mutate the state of the UI components.
+                    //
+                    // However, this crazy 'thread flip' (or mount) causes UI.getCurrent() to return null. We'll fix that by remembering the
+                    // current UI for the carrier thread, then we'll look it up from the virtual thread and set it as current.
+                    // CURRENT_UI is going to help us with that.
+                    command.run();
+                } finally {
+                    CURRENT_UI.remove(carrierThread);
+                }
+            });
+        };
+
+        // construct a specialized virtual thread builder which runs continuations on uiExecutor
         try {
             final Class<?> vtbclass = Class.forName("java.lang.ThreadBuilders$VirtualThreadBuilder");
             final Constructor<?> c = vtbclass.getDeclaredConstructor(Executor.class);
             c.setAccessible(true);
-            Thread.Builder.OfVirtual vtb = (Thread.Builder.OfVirtual) c.newInstance((Executor) command -> {
-                // "command" is a continuation which runs another piece of the virtual thread.
-                ui.access((Command) () -> {
-                    // current thread will become a carrier thread once command.run() is run.
-                    CURRENT_UI.put(Thread.currentThread(), UI.getCurrent());
-                    try {
-                        // This calls a JVM Continuation. Upon mounting a continuation,
-                        // the current thread is switched to a virtual thread (!!). The current (carrier) thread is blocked while executing virtual thread,
-                        // which means that the virtual thread also runs in the UI thread and is safe to mutate the state of the UI components.
-                        //
-                        // However, this crazy 'thread flip' (or mount) causes UI.getCurrent() to return null. We'll fix that by remembering the
-                        // current UI for the carrier thread, then we'll look it up from the virtual thread and set it as current.
-                        // CURRENT_UI is going to help us with that.
-                        command.run();
-                    } finally {
-                        CURRENT_UI.remove(Thread.currentThread());
-                    }
-                });
-            });
+            final Thread.Builder.OfVirtual vtb = (Thread.Builder.OfVirtual) c.newInstance(uiExecutor);
             return vtb;
         } catch (Exception e) {
             throw new RuntimeException(e);
@@ -99,6 +106,7 @@ public final class UIExecutor implements AutoCloseable {
 
     /**
      * Maps a carrier thread to the Vaadin UI which currently holds session lock in that carrier thread.
+     * Used to set current Vaadin UI in the virtual thread.
      */
     @NotNull
     private static final ConcurrentHashMap<Thread, UI> CURRENT_UI = new ConcurrentHashMap<>();
@@ -108,7 +116,10 @@ public final class UIExecutor implements AutoCloseable {
      * every time you block in an unpinned way (when the virtual thread is unmounted and mounted back).
      */
     public static void applyVaadin() {
-        assertVirtualThread();
+        // find out the correct UI instance from the carrier thread and set it to this thread.
+        // The carrier thread holds the UI lock and is blocked running this virtual thread, therefore
+        // we can conclude that the virtual thread holds the UI lock as well. Therefore,
+        // there's no need to lock the UI lock again.
         final Thread carrierThread = currentCarrierThread();
         // the carrier thread has the current UIs set properly.
         UI ui = CURRENT_UI.get(carrierThread);
@@ -117,8 +128,9 @@ public final class UIExecutor implements AutoCloseable {
         }
         UI.setCurrent(ui);
         VaadinSession.setCurrent(ui.getSession());
+
         // post-check: make sure everything is set correctly.
-        UIExecutor.assertUIVirtualThread();
+        assertUIVirtualThread();
     }
 
     /**
@@ -126,6 +138,7 @@ public final class UIExecutor implements AutoCloseable {
      */
     @NotNull
     private static Thread currentCarrierThread() {
+        assertVirtualThread();
         try {
             final Class<?> cc = Class.forName("jdk.internal.vm.Continuation");
             final Method m = cc.getDeclaredMethod("currentCarrierThread");
@@ -136,11 +149,20 @@ public final class UIExecutor implements AutoCloseable {
         }
     }
 
-    public static void assertVirtualThread() {
+    /**
+     * Asserts that this thread is a virtual one.
+     */
+    private static void assertVirtualThread() {
         if (!Thread.currentThread().isVirtual()) {
             throw new IllegalStateException("This can only be called from closures run via loom()");
         }
     }
+
+    /**
+     * Asserts that this thread is a virtual thread which is run in the Vaadin UI thread.
+     * It's useful to assert this before attempting to block, to make sure the
+     * blocking operation suspends current virtual thread instead.
+     */
     public static void assertUIVirtualThread() {
         assertVirtualThread();
         if (UI.getCurrent() == null) {
