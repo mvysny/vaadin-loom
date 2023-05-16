6721e6838f9d52975c7e55d8f159c54a38060dfc
Martin Vysny <mavi@vaadin.com>
2023-05-16 12:21:15 +0300

    Remove unused stuff

--- src/main/java/com/vaadin/starter/skeleton/AppShell.java
+++ src/main/java/com/vaadin/starter/skeleton/AppShell.java
@@ -2,11 +2,7 @@ package com.vaadin.starter.skeleton;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
-import com.vaadin.flow.server.PWA;
-import com.vaadin.flow.theme.Theme;
 
-@PWA(name = "Project Base for Vaadin", shortName = "Project Base")
-@Theme("my-theme")
 @Push
 public class AppShell implements AppShellConfigurator {
 }
