5c920167441a3cf8a33de345b52402b351728dd9
Martin Vysny <mavi@vaadin.com>
2025-03-21 09:12:22 +0200

    vaadin v bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,9 +38,9 @@ dependencies {
 java {
     sourceCompatibility = JavaVersion.VERSION_21
     targetCompatibility = JavaVersion.VERSION_21
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
-    }
+//    toolchain {
+//        languageVersion = JavaLanguageVersion.of(21)
+//    }
 }
 
 tasks.withType<Test> {
